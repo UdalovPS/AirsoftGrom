b87e56ceea44c443fcceb2aeaba52ce027920365
UdalovPS <udalovpavel95@gmail.com>
2022-08-28 15:13:51 +0500

    add start message for nrf quest

--- telebot/main.py
+++ telebot/main.py
@@ -15,7 +15,8 @@ logging.basicConfig(level=logging.INFO)
 @dp.message_handler(commands='фа5083')
 async def reg_msg(message: types.Message):
     """blue start point for quest"""
-    text = QuestsDb().select_text_for_nrf_quest(1, int(message.text[2:]))
+    # text = QuestsDb().select_text_for_nrf_quest(1, int(message.text[2:]))
+    text = message.text[2:]
     await message.answer(text)
 
 @dp.message_handler(commands='ми5083')
